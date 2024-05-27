4df6874e6ab62846b0710a63fcaf5dcac83d57f5
Kim ByoungChan <kimbc814@gmail.com>
2024-05-27 09:28:35 +0900

    add message transmission/reception function of 'CAN fieldbus relay module'.

--- CAN_FIFO_Module/CAN_Console.cpp
+++ CAN_FIFO_Module/CAN_Console.cpp
@@ -8,6 +8,9 @@
 #include <mutex>
 #include <chrono>
 #include <vector>
+#include <sstream>
+#include <string>
+#include <cstring>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -23,13 +26,41 @@ typedef struct msg_frame {
 msgFrame* frames[MAX_FRAMES] = { nullptr, };
 std::mutex frames_mutex;
 
-void SendCanMessage(CAN_HANDLE h) {
-    long sid = 0;
-    unsigned char sdata[8] = { 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00, 0x11 };
+std::vector<std::string> can_send_parts;
+std::string can_send_part;
+void SendCanMessage(CAN_HANDLE h, const char *inputdata) {
+    can_send_parts.clear();
+    std::istringstream ss(inputdata);
+    while (getline(ss, can_send_part, ' ')) {
+        can_send_parts.push_back(can_send_part);
+    }
+    if (can_send_parts.size() != 10) {
+        printf("%d\n", can_send_parts.size());
+        printf("Error: Invalid input format. Expected 9 parts.\n");
+        return ;
+    }
+
+    long sid = std::strtol(can_send_parts[0].c_str(), nullptr, 16);
+
+    unsigned char sdata[8];
+    for (size_t i = 0; i < 8; ++i) {
+        sdata[i] = static_cast<char>(std::strtol(can_send_parts[i + 2].c_str(), nullptr, 16));
+    }
 
     int ret1 = CAN_Send(h, sid, 8, (char*)sdata, 0, 0);
     if (!ret1) {
         printf("Send failed.\n");
+    }
+    else {
+        printf("CAN SEND: %03X 8 %02X %02X %02X %02X %02X %02X %02X %02X\n", sid, 
+            static_cast<unsigned char>(sdata[0]),
+            static_cast<unsigned char>(sdata[1]),
+            static_cast<unsigned char>(sdata[2]),
+            static_cast<unsigned char>(sdata[3]),
+            static_cast<unsigned char>(sdata[4]),
+            static_cast<unsigned char>(sdata[5]),
+            static_cast<unsigned char>(sdata[6]),
+            static_cast<unsigned char>(sdata[7]));
     }
 }
 
@@ -64,23 +95,8 @@ void RecvCanMessage(CAN_HANDLE h) {
     }
     else { printf("CAN READ FAIL\n"); }
 }
-void threadReadCAN() {
-    // CAN 핸들 설정
-    int noDevice = CAN_Fifo_ScanSerialNumber();
-    if (noDevice <= 0) {
-        printf("Can't find CAN Module\n");
-        exit(0);
-    }
-
-    CAN_HANDLE h = CAN_OpenFifo(CAN_Fifo_GetSerialNumber(0));
-    if (h < 0) {
-        printf("USB2CAN open failed.\n");
-        exit(0);
-    }
-
-    HANDLE hEvent = CreateEvent(NULL, false, false, "");
-    CAN_SetRxEventNotification(h, hEvent);
-    CAN_SetTransferMode(h, 1);
+void threadReadCAN(CAN_HANDLE h) {
+    
 
     while (1) {
         if (CAN_CountRxQueue(h) > 0) {
@@ -90,10 +106,32 @@ void threadReadCAN() {
     }
     // 정리
     CAN_Close(h);
-    CloseHandle(hEvent);
+    
 }
+#define PACKET_SIZE 30
+void threadReadSock(CAN_HANDLE h, SOCKET sock) {
+    char buffer[PACKET_SIZE] = {}; //char 생성
+    while (!WSAGetLastError()) {
+        ZeroMemory(&buffer, PACKET_SIZE); //buffer 비우기
+        int bytesRead = recv(sock, buffer, PACKET_SIZE-1, 0); //데이터받아오기
+        if (bytesRead > 0) {
+            buffer[PACKET_SIZE-1] = '\0';
+            printf("Socket Recv: %s\t", buffer);
+            SendCanMessage(h, buffer);
+        }
+
+    }
+}
+
 void threadSendSock(SOCKET sock) {
     while (1) {
+        send(sock, "301 8 01 00 00 00 00 00 00 00", strlen("101 8 42 44 00 00 00 00 00 00"), 0);
+        //printf("%d\n", strlen("01 01 42 44 00 00 00 00 00 00"));
+        Sleep(1000);
+        send(sock, "301 8 04 00 00 00 00 00 00 00", strlen("101 8 42 44 00 00 00 00 00 00"), 0);
+        //printf("%d\n", strlen("01 FF FF 00 00 00 00 00 00 00"));
+        Sleep(1000);
+        
         /*
         send(sock, "316 8 35 99 EC 18 99 19 20 7D", strlen("316 8 35 99 EC 18 99 19 20 7D"), 0);
         Sleep(10);
@@ -110,6 +148,7 @@ void threadSendSock(SOCKET sock) {
         send(sock, "316 8 35 99 68 10 99 19 20 7D", strlen("316 8 35 99 EC 18 99 19 20 7D"), 0);
         Sleep(10);
         */
+        /*
         //DWORD dwWaitResult = WaitForSingleObject(hEvent, 1);
         std::vector<msgFrame> local_frames;
         {
@@ -148,8 +187,9 @@ void threadSendSock(SOCKET sock) {
             // 10ms 대기
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
-        
+        */
     }
+
 }
 int _tmain(int argc, _TCHAR* argv[]) {
     // Winsock 초기화
@@ -181,13 +221,30 @@ int _tmain(int argc, _TCHAR* argv[]) {
             Sleep(1000);
         }
     }
-    
-    std::thread read_CAN(threadReadCAN);
+    // CAN 핸들 설정
+    int noDevice = CAN_Fifo_ScanSerialNumber();
+    if (noDevice <= 0) {
+        printf("Can't find CAN Module\n");
+        exit(0);
+    }
+
+    CAN_HANDLE h = CAN_OpenFifo(CAN_Fifo_GetSerialNumber(0));
+    if (h < 0) {
+        printf("USB2CAN open failed.\n");
+        exit(0);
+    }
+
+    HANDLE hEvent = CreateEvent(NULL, false, false, "");
+    CAN_SetRxEventNotification(h, hEvent);
+    CAN_SetTransferMode(h, 1);
+
+    std::thread read_CAN(threadReadCAN,h);
     std::thread send_sock(threadSendSock, sock);
+    std::thread read_sock(threadReadSock,h, sock);
     read_CAN.join();
     send_sock.join();
 
-    
+    CloseHandle(hEvent);
     closesocket(sock);
     WSACleanup();
 
